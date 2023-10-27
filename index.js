130be0e46b26b5fef6025e0bf6c03092f7687b2c
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-10-27 17:01:10 +0200

    start welcome func

--- index.js
+++ index.js
@@ -3,8 +3,29 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 import gradient from "gradient-string";
-import chalkanimation from "chalk-animation";
+import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
 
-console.log(chalk.bgGreen("hi, mom"));
+console.log(chalk.bgRed("Functionality achieved!"));
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow("Who wants to be a JavaScript Millionaire?\n");
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.log(`
+    ${chalk.bgMagenta("HOW TO PLAY")}
+    I am a process on your computer.
+    If you get any questions wrong I will be ${chalk.bgRedBright("KILLED...")}
+    So please get all the questions right...
+    
+  `);
+}
+
+welcome();
