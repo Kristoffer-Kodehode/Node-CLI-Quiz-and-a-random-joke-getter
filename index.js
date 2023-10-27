d76c8675a0fc0a445b67334b79d892a51d87fa27
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-10-27 16:47:18 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+//imports
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkanimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+console.log(chalk.bgGreen("hi, mom"));
